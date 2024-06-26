46afd259d9231faacb93f4bb5a955f7b3c9a9b3c
123valery <113314173+123valery@users.noreply.github.com>
2024-06-25 23:02:57 -0400

    comienzo del main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import pickle
 from Equipo import Equipo
 from Partido import Partido
 from Estadio import Estadio
@@ -10,6 +11,7 @@ from General import General
 from VIP import VIP
 from Vampiro import es_vampiro
 from Perfecto import es_perfecto
+from tabulate import tabulate
 
 def api_manager():
     '''descarga la informacion del programa de la API en GitHub'''
@@ -541,8 +543,456 @@ def restaurant_manager(restaurant, client,ticket):
     print('descuentos: $',discount)
     print(f'TOTAL: ${total_f}')
 
-while True:
+    while True:
+        try:
+            proceed = int(input('\nDesea finalizar la compra?\n1 - Si\n2 - No\n+> '))
+            if proceed != 1 and proceed != 2:
+                raise Exception
+            else:
+                break
+        except:
+            print('\nopcion invalida\n')
+
+    if proceed ==1:
+        print('\n---RESUMEN DE COMPRA---')
+        print(f'subtotal: ${sub_total}')
+        print(f'descuento: ${discount}')
+        ticket.total += total
+        for item in cart:
+            for product in restaurant.products:
+                if item.name == product.name:
+                    product.quantity -=1
+        #appendear a lista o updetear gastos cliente para estadisticas
+        print('\nEl pago se ha completadp exitosamente!\n')
+    else:
+        print('\nLa compra ha sido cancelada!\n')
+    return restaurant, client
+
+def search_by_name(restaurants):
+    '''funcion de busqueda de prodcutos en restaurante por nombre'''
+    product_names = []
+    print('\n--PRODUCTOS DISPONIBLES--')
+    for restaurant in restaurants:
+        for product in restaurant.products:
+            if len(product_names) ==0:
+                product_names.append(product.name)
+            else:
+                found = False
+                for product_name in product_names:
+                    if product_name == product.name:
+                        found = True
+                        break
+                if found == False:
+                    product_names.append(product.name)
+    for name in product_names:
+        print(name)
+    while True:
+        try:
+            food = input('Ingrese el producto que quiere buscar en restaurantes:  ').lower()
+            found = False
+            for restaurant in restaurants:
+                for product in restaurant.products:
+                    if food == product.name.lower():
+                        found = True
+                        break
+            if found == False:
+                raise Exception
+            else:
+                break
+        except:
+            print('\ningreso invalido! intente de nuevo\n')
+    for restaurant in restaurants:
+        print('\n---', restaurant.name.upper(), '----')
+        found = 0
+        for product in restaurant.products:
+            if food == product.name.lower():
+                found +=1
+                product.show()
+        if found == 0:
+            print('\n Producto no disponible en restaurante')
+
+def search_by_type(restaurantes): #REVISAR
+    '''funcion de busqueda de productos en restaurante por tipo alimento/bebida'''
+    while True:
+        try:
+            type_chosen = int(input('\nQue tipo de productos desea buscar?\n1-Bebidas\n2-Comidas\n=> '))
+            if type_chosen != 1 and type_chosen != 2:
+                raise Exception
+            else:
+                break
+        except:
+            print('\nSeleccion invalida! Intente de nuevo \n')
+    if type_chosen == 1:
+        for restaurante in restaurantes:
+            print('\n----', restaurante.name.upper(), '---')
+            for product in restaurante.products:
+                if product.adicional == 'plate' or product.adicional =='package':
+                    product.show()
     
+    if type_chosen ==2:
+        for restaurante in restaurantes:
+            print('\n----', restaurante.name.upper(), '---')
+            for product in restaurante.products:
+                if product.adicional == 'alcoholic' or product.adicional =='non-alcoholic':
+                    product.show()
+
+def search_by_price_range(restaurantes):
+    '''funcion de busqueda de productos en restaurante por rango de precio'''
+    while True:
+        try:
+            price_range_top = int(input('Indique el precio maximo por producto de su busqueda: $'))
+            if price_range_top >997:
+                print('\nconsidere bajar el rango de precio (max 997$)')
+                raise Exception
+            if price_range_top == 0:
+                raise Exception
+            break
+        except:
+            print('\nrango no valido, intente de nuevo\n')
+    
+    while True:
+        try:
+            price_range_low= int(input('Indique el precion minimo por prodicto de su busqueda:  $ '))
+            if price_range_low < 1:
+                print('\nconsidere subir el rango de precio (min 1$)')
+                raise Exception
+            break
+        except:
+            print('\nrango no valido, intente de nuevo\n')
+    
+
+    for restaurante in restaurantes:
+        print('\n\n---', restaurante.name.upper(),'---')
+        found = 0
+        for product in restaurante.products:
+            if price_range_low <= product.price<=price_range_top:
+                found +=1
+                product.show()
+        if found == 0:
+            print('\nNo hay productos en el rango de precio elegido')
+
+def get_ticket_used(partidos, ticekts_vendidos):
+    '''agrega los tickets usados al atributo de cada partido'''
+    for partido in partidos:
+        for ticket in ticekts_vendidos:
+            if ticket.match.id == partido.id:
+                if ticket.used:
+                    partido.used+=1
+    return partidos, ticekts_vendidos
+
+def get_average_vip(clients):
+    '''calcula el gasto rpomedio de un cliente VIP por cada partido'''
+    vip_expenses = []
+    for client in clients:
+        for ticket in client.tickets:
+            if ticket.price == 75:
+                vip_expenses.append(ticket.total)
+    if len(vip_expenses) ==0:
+        return 0
+    else:
+        average = sum(vip_expenses)/len(vip_expenses)
+        average_f = '{:.2f}'.format(average)
+        return average_f
+    
+def match_assistance(partidos):
+    '''ordena los partidos segun asistencia y muestra una tabla de todos los partidos'''
+    print('\n--ASISTENCIA DE PARTIDOS--')
+    print('se muestra: equipos - estadio - tickets vendidos - asistencia\n')
+    matches = []
+    for partido in partidos:
+        if len(matches)== 0:
+            matches.append(partido)
+        else:
+            for match in matches:
+                if partido.used >= match.used:
+                    matches.insert(0, partido)
+                    break
+                else:
+                    matches.append(partido)
+    
+    data = []
+    for match in matches:
+        data.append([f'{match.home_team.name} vs {match.away_team.name}', match.stadium_id.name, match.tickes_sold, match.used])
+    info = ['EQUIPOS', 'ESTADIO', 'VENDIDOS', 'ASISTENCIA']
+    print(tabulate(data, headers = info))
+
+def most_tickets_sold(partidos):
+    '''calcula y muestra el partido con mayor cantidad de tickets vendidos'''
+    most_sold = None
+    for partido in partidos:
+        if most_sold == None:
+            most_sold = partido
+        else:
+            if partido.tickets_sold>most_sold.tickets_sold:
+                most_sold = partido
+    print(f'''\n\n--PARTIDO CON MAS TICKETS VENDIDOS--
+                ---{most_used.home_team.name} vs {most_used.away_team.name}----
+                    fecha: {most_used.date}
+                    estadio: {most_used.stadium_id.name}
+                    asistencia: {most_used.used}''')
+
+def top_3_restaurants(restaurants): #REVISAR
+    '''calcula y muestra los 3 productos mas comprados en todos los restaurantes de la base de datos '''
+    products_sold = []
+    for restaurant in restaurants:
+        for product in restaurant.products:
+            if len(products_sold)==0:
+                product.sold = 25 - product.quantity #AQUI
+                products_sold.append(product)
+            else:
+                found = False
+                for p in products_sold:
+                    if product.name == p.name:
+                        p.sold+=(25-product.quantity)
+                        found = True
+                        break
+                if found == False:
+                    product.sold = 25-product.quantity
+                    products_sold.append(product)
+    product1= None
+    product2= None
+    product3= None
+
+    for p in products_sold:
+        if product1 == None:
+            product1 = p
+        else:
+            if p.sold>product1.sold:
+                product1 = p
+
+    for p in products_sold:
+        if p.name == product1.name:
+            continue
+        elif product2 == None:
+            product2= p
+        else:
+            if p.sold>product2.sold:
+                product2=p
+
+    for p in products_sold:
+        if p.name == product1.name:
+            continue
+        elif p.name == product2.name:
+            continue
+        elif product3==None:
+            product3 = p
+        else:
+            if p.sold>product3.sold:
+                product3 = p
+    print(f'''\n\n---PRODUCTOS MAS VENDIDOS EN LOS RESTAURANTES---
+                1 - {product1.name}: {product1.sold}
+                2 - {product2.name}: {product2.sold}
+                3 - {product3.name}: {product3.sold}''')
+
+def top_3_clients(clients):
+    '''muestra los 3 clientes con mayor cantidad de ticekts comprados, si hay menos de 3 clientes muestra el top 2 o top 1 respectivamente'''
+    client1 = None
+    client2 = None
+    client3 = None
+
+    for client in clients:
+        if client1 == None:
+            client1 = client
+        else:
+            if len(client.tickets)>len(client1.tickets):
+                if client2 != None:
+                    client3 = client2
+                client2 = client1
+                client1 = client
+            else:
+                client3= client2
+                client2 = client
+    print(f'''\n--CLIENTES CON MAS BOLETOS COMPRADOS--
+                1-{client1.name}
+                cedula: {client1.cedula}
+                boletos: {len(client1.tickets)}''')
+    
+    if client2==None:
+        pass
+    else:
+        print(f''' 2- {client2.name}
+                cedula : {client2.cedula}
+                boletos: {len(client2.tickets)}''')
+    
+    if client3==None:
+        pass
+    else:
+        print(f''' 3- {client3.name}
+                cedula : {client3.cedula}
+                boletos: {len(client3.tickets)}''')
+
+def main():
+    '''funcion principal del programa'''
+    print('\n---BIENVENIDO A LA VENTA DE TICKETS DE LA EUROCOPA DE FUTBOLA EURO2024---\n')
+    load = input('Seleccione una opcion para la descarga de datos:\n1 - Cargar datos anteriores\2 - Reiniciciar programa \n=>')
+    while not load.isnumeric() or int(load) !=1 and int(load) !=2:
+        load = input('\nSeleccion invalida!\nSeleccione una opcion para la descarga de datos:\n1 - Cargar datos anteriores\2 - Reiniciciar programa \n=>')
+    
+    if int(load)==1:
+        try:
+            estadios = pickle.load(open('Estadios.txt','rb'))
+        except E0FError:
+            estadios = list()
+        
+        try:
+            equipos = pickle.load(open('Equipos.txt','rb'))
+        except E0FError:
+            equipos = list()
+        
+        try:
+            partidos = pickle.load(open('Partidos.txt','rb'))
+        except E0FError:
+            partidos = list()
+        
+        try:
+            clientes = pickle.load(open('Clientes.txt','rb'))
+        except E0FError:
+            clientes = list()
+        
+        try:
+            tickets_vendidos = pickle.load(open('Tickets_vendidos.txt','rb'))
+        except E0FError:
+            tickets_vendidos = list()
+
+        try:
+            restaurantes = pickle.load(open('Restaurantes.txt','rb'))
+        except E0FError:
+            restaurantes = list()
+
+        try:
+            codigos_tickets = pickle.load(open('Codigos_tickets.txt','rb'))
+        except E0FError:
+            codigos_tickets= list()
+        
+        try:
+            codigos_usados = pickle.load(open('Codigos_usados.txt','rb'))
+        except E0FError:
+            codigos_usados= list()
+    
+    else:
+        info_equipos, info_partidos, info_estadios = api_manager()
+        equipos = manage_equipos(info_equipos)
+        partidos = manage_partidos(info_partidos)
+        estadios = manage_estadios(info_estadios)
+        estadios, restaurantes = manage_restaurantes(estadios)
+        partidos = manage_objects_partidos(partidos, equipos, estadios)
+        estadios, restaurantes = add_iva_products(estadios, restaurantes)
+        clients = []
+        tickets_vendidos = []
+        codigos_tickets = []
+        codigos_usados = []
+
+    while True:
+        print('\n---BIENVENIDO A LA VENTA DE TICKETS DE LA EUROCOPA DE FUTBOL EURO 2024---\n')
+        option = get_option
+
+        if option ==1:
+
+            while True:
+                try:
+                    registrado = int(input('\nUsted se encuentra registrado en el sistema?\n1 - Si\n2 - No\n=> '))
+                    if registrado != 1 and registrado != 2:
+                        raise Exception
+                    else:
+                        break
+                except:
+                    print('\nOpcion invalida\n')
+            if int(registrado)==1:
+                clients, client = find_client(clients)
+            else:
+                clients, client = register_client(clients)
+            
+            print('\nPARTIDOS DISPONIBLES')
+            for partido in partidos:
+                capacity_stadium = partido.stadium_id.capacity
+                seat_amount = int(capacity_stadium[0]) + int(capacity_stadium[1])
+                sold = []
+                for ticket in tickets_vendidos:
+                    if ticket.match == partido:
+                        sold.append(ticket)
+                if len(sold)<seat_amount:
+                    partido.show()
+            match_selected = select_match(partidos)
+            ticket_selected = ticket_selection(match_selected, tickets_vendidos)
+            for partido in partidos:
+                if match_selected.id == partido.id:
+                    ticket_selected = seat_selection(match_selected, partido, tickets_vendidos)
+            ticket_selected = ticket_total(ticket_selected, client) 
+            client, codigos_tickets, tickets_vendidos, match_selected = buy_ticket(client, ticket_selected, codigos_tickets, tickets_vendidos, match_selected,)                         
+
+        elif option ==2:
+            print('\n--- MODULO ADMINISTRATIVO DE USO DE ENTRADAS ---')
+            while True:
+                if len(tickets_vendidos) == 0:
+                    print('\ntodavia no se han realizado compras en el sistema!')
+                    break
+                else:
+                    codigos_usados, codigos_tickets,tickets_vendidos = ticket_manager(codigos_usados, codigos_tickets, tickets_vendidos)
+                    break
+        elif option ==3:
+            print('---- BIENVENIDO AL BUSCADOR DE PARTIDOS ----')
+            while True:
+                try:
+                    search = int(input('\nomo desea busacr los partidos?\n1 - Pais\n2 - Estadios\n3 - Fecha \n=> '))
+                    if search != 1 and search != 2 and search != 3:
+                        raise Exception
+                    else:
+                        break
+                except:
+                    print('\nopcion invalida\n')
+            if search == 1:
+                search_by_country(equipos, partidos)
+            elif search ==2:
+                search_by_stadium(estadios, equipos)
+            else:
+                search_by_date(equipos, partidos)
+        
+        elif option== 4:
+            print('\n     --- BIENVENIDO A LA SELECCION DE RESTAURANTES --- ')
+            print('Para poder continuar debe haber adquirido una entrada VIP con antelacion')
+            cedula = get_cedula()
+            while True:
+                if len(clients)==0:
+                    print('\n=> Todavia no se han realizado compras en el sistema\n')
+                for client in clients:
+                    if cedula == client.cedula:
+                        if len(client.tickets)==0:
+                            print(client.name, 'todavia no has comrpado entradas!\n')
+                            break
+                        vip_tickets = []
+                        for ticket in client.tickets:
+                            if ticket.price == 75:
+                                vip_tickets.append(ticket)
+                        if len(vip_tickets) != 0:
+                            print('\nAl restaurante de que partido desea ingresar? ')
+                            count = 1
+
+                            for ticket_vip in vip_tickets:
+                                print('\nTICKET', count)
+                                print(ticket_vip.match.show()) #HAY UN NONE NO SE DE DONDE SALE
+                                count +=1
+                            
+                            while True:
+                                try:
+                                    choice = int(input('\n Ingrese el id del partido de su preferencia para ingresar al restaurante del estadio: '))
+                                    found = False
+                                    for vip_ticket in vip_tickets:
+                                        if choice == int(vip_ticket.match.id):
+                                            found = True
+                                            break
+                                        if found == False:
+                                            raise Exception
+                                        break
+                                except:
+                                    print('\ningreso invalido, intente de nuevo!')
+
+
+
+        
+
+
+
+
 
 
 
